9f1ac0c552773c692952f75320d30204a619bcc1
u.baibikov <u.baibikov@mediasoft.team>
2023-03-04 21:16:39 +0400

    add logic

---
+++ brain_games/games/gcd_games.py
@@ -0,0 +1,29 @@
+import prompt 
+from random import randint
+import math
+
+
+BEGIN = 1
+END = 100
+
+
+def gcd_play():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Find the greatest common divisor of given numbers.')
+    count = 0
+    while count < 3:
+        divisor1 = randint(BEGIN, END)
+        divisor2 = randint(BEGIN, END)
+        print(f'Question: {divisor1} {divisor2}')
+        correct_answer = math.gcd(divisor1, divisor2)
+        answer = prompt.string('Your answer: ')
+        if correct_answer != int(answer):
+            print(f'''"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}".\n Let's try again, {name}!''')
+            break
+        else:
+            print('Correct!')
+            count += 1
+    if count == 3:
+      print(f'Congratulations, {name}!')
