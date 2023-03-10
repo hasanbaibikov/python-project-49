82158a1ddacd4ecab523cef543b976e426f90717
u.baibikov <u.baibikov@mediasoft.team>
2023-03-10 18:35:06 +0400

    another fix.

--- brain_games/games/progression_games.py
+++ brain_games/games/progression_games.py
@@ -8,7 +8,7 @@ def ar_progression_play():
     print(f'Hello, {name}!')
     print("What number is missing in the progression?") 
     count = 0
-    LEN = 11
+    LEN = 10
     while count < 3:
         start = random.randint(1, 100)
         step = random.randint(1, 5)
@@ -17,10 +17,10 @@ def ar_progression_play():
         d = step
         for i in range(0, LEN):
             my_list.append(str(a+i*d))
-        rand_int = random.randint(0, LEN)
+        rand_int = random.randint(0, LEN - 1)
         correct_answer = my_list[rand_int]
         my_list[rand_int] = '..'
-        progression = ', '.join(my_list)
+        progression = ' '.join(my_list)
         print(f'Question: {progression}')
         answer = prompt.string('Your answer: ')
         if correct_answer != answer:
@@ -30,4 +30,4 @@ def ar_progression_play():
           print('Correct!')
           count += 1
     if count == 3:
-      print(f'Coungratilations, {name}!')
+      print(f'Congratulations, {name}!')
