d0e83151385fec5adc57b7f23c2cbc7779d7d3a0
u.baibikov <u.baibikov@mediasoft.team>
2023-03-27 14:05:17 +0400

    Update calc_games.py

--- brain_games/games/calc_games.py
+++ brain_games/games/calc_games.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+import operator
 
 
 GAME_MESSAGE = 'What is the result of the expression?'
@@ -7,22 +8,22 @@ MAX_NUMBER = 10
 OPERATORS = ['+', '-', '*']
 
 
-def calc_expression(num1, num2, operator):
-    if operator == '+':
-        crt = num1 + num2
+def calc_expression(num1, num2, random_operator):
+    if random_operator == '+':
+        crt = operator.add(num1, num2)
         return crt
-    elif operator == '-':
-        crt = num1 - num2
+    elif random_operator == '-':
+        crt = operator.sub(num1, num2)
         return crt
     else:
-        crt = num1 * num2
+        crt = operator.mul(num1, num2)
         return crt
 
 
 def get_game():
     num1 = randint(MIN_NUMBER, MAX_NUMBER)
     num2 = randint(MIN_NUMBER, MAX_NUMBER)
-    operator = choice(OPERATORS)
-    question = f'Question: {num1} {operator} {num2}'
-    crt = calc_expression(num1, num2, operator)
+    random_operator = choice(OPERATORS)
+    question = f'Question: {num1} {random_operator} {num2}'
+    crt = calc_expression(num1, num2, random_operator)
     return question, str(crt)
