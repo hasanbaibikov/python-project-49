6bcd642d02d53e837e3d8b354b58374f0adf265f
u.baibikov <u.baibikov@mediasoft.team>
2023-03-30 13:50:52 +0400

    calc_expression now returns an operator function instead of a number.

--- brain_games/games/calc_games.py
+++ brain_games/games/calc_games.py
@@ -5,25 +5,20 @@ import operator
 GAME_MESSAGE = 'What is the result of the expression?'
 MIN_NUMBER = 1
 MAX_NUMBER = 10
-OPERATORS = ['+', '-', '*']
+OPERATORS = {'+': operator.add,
+             '-': operator.sub,
+             '*': operator.mul}
 
 
-def calc_expression(num1, num2, random_operator):
-    if random_operator == '+':
-        crt = operator.add(num1, num2)
-        return crt
-    elif random_operator == '-':
-        crt = operator.sub(num1, num2)
-        return crt
-    else:
-        crt = operator.mul(num1, num2)
-        return crt
+def calc_expression(random_operator):
+    return OPERATORS.get(random_operator)
 
 
 def get_game():
     num1 = randint(MIN_NUMBER, MAX_NUMBER)
     num2 = randint(MIN_NUMBER, MAX_NUMBER)
-    random_operator = choice(OPERATORS)
+    random_operator = choice(list(OPERATORS.keys()))
     question = f'Question: {num1} {random_operator} {num2}'
-    crt = calc_expression(num1, num2, random_operator)
+    operator_func = calc_expression(random_operator)
+    crt = operator_func(num1, num2)
     return question, str(crt)
