8f205308b40d01933116a3a3d14f5051ad8b1a11
u.baibikov <u.baibikov@mediasoft.team>
2023-03-30 13:52:49 +0400

    the number of attempts is now displayed as a constant, at the beginning of the module.

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -4,13 +4,16 @@
 import prompt
 
 
+TRIES = 3
+
+
 def run(game):
+    global TRIES
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.GAME_MESSAGE)
-    tries = 3
-    while tries:
+    while TRIES:
         question, crt = game.get_game()
         print(question)
         answer = prompt.string('Your answer: ')
@@ -20,5 +23,5 @@ def run(game):
             return
         else:
             print('Correct!')
-            tries -= 1
+            TRIES -= 1
     print(f'Congratulations, {name}!')
