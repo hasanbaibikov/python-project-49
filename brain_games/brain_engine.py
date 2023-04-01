7d1456ed788425a5dba5ada1da36a822157b7dac
u.baibikov <u.baibikov@mediasoft.team>
2023-04-01 17:13:45 +0400

    Rewrote the constant without using a global variable and left the shebang only in executable files

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -1,19 +1,16 @@
-#!/usr/bin/python3
-
-
 import prompt
 
 
-TRIES = 3
+MAX_TRIES = 3
 
 
 def run(game):
-    global TRIES
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.GAME_MESSAGE)
-    while TRIES:
+    tries_left = MAX_TRIES
+    while tries_left:
         question, crt = game.get_game()
         print(question)
         answer = prompt.string('Your answer: ')
@@ -23,5 +20,5 @@ def run(game):
             return
         else:
             print('Correct!')
-            TRIES -= 1
+            tries_left -= 1
     print(f'Congratulations, {name}!')
